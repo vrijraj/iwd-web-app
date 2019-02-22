b3df7440db536b38b311c70b7e49764fd25719c8
Vrijraj Singh <vrijraj2396@gmail.com>
2019-02-22 16:13:01 +0530

    Home Data Binding

--- src/components/partners.vue
+++ src/components/partners.vue
@@ -8,12 +8,11 @@
             <v-flex xs12 md12 class="mt-2">
                 <p class="google-font mt-2" >Genral Creator</p>
                 
-                <v-layout row wrap>
+                <v-layout wrap align-center justify-start row fill-height>
                     <v-flex xs6 sm2 md2 lg2 v-for="itemp in partnerData" :key="itemp.name" class="pa-1">
                         <a v-bind:href="itemp.link" target="_blank">
-                            <div class="elevation-1 pa-2">
-                                {{itemp.img}}
-                                <img :src="itemp.img" width="100%" alt="" srcset="">
+                            <div class="elevation-1 pa-3 text-xs-center" style="height:90px;">
+                                <img :src="getImgUrl(itemp.img)" width="80%" alt="" srcset="">
                             </div>
                         </a>
                     </v-flex>
@@ -26,18 +25,13 @@
                 <v-layout row wrap class="mt-0">
                     <v-flex xs6 sm2 md2 lg2>
                         <a href="https://gdgjalandhar.com" target="_blank">
-                            <div class="elevation-1 pa-2">
-                                <img src="@/assets/imgs/partners/gdgjalandhar.png" width="100%" alt="" srcset="">
+                            <div class="elevation-1 pa-2 text-xs-center" style="height:90px;">
+                                <img src="@/assets/imgs/partners/gdgjalandhar.png" width="90%" alt="" srcset="">
                             </div>
                         </a>
                     </v-flex>
                 </v-layout>
             </v-flex>
-
-        
-
-
-           
         </v-layout>
     </v-container>
 </template>
@@ -48,8 +42,12 @@ export default {
     data() {
         return {
             partnerData: datap,
-            imgPath:'@/assets/imgs/partners/'
         }
     },
+    methods:{
+        getImgUrl(pic) {
+            return require('../assets/imgs/partners/'+pic)
+        }
+    }
 }
 </script>
