56c33dac71d351a298be0520b2f39190d60f1a95
Vrijraj Singh <vrijraj2396@gmail.com>
2019-02-22 15:08:52 +0530

    Home Data Binding

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,12 +1,16 @@
 <template>
-    <v-container fluid class="mt-5 text-xs-center hidden-md-and-down">
+    <v-footer class="pa-3 hidden-md-and-down">
+        <v-spacer></v-spacer>
+        <div>&copy; Designed & Developed By <a href="https://gdgjalandhar.com" target="_blank">GDG Jalandhar</a></div>
+    </v-footer>
+    <!-- <v-container fluid class="mt-5 text-xs-center hidden-md-and-down">
         <v-layout wrap align-center justify-center row fill-height>
             <v-flex xs12 md12>
                 <v-divider></v-divider>
                 <p class="mt-3 google-font subheading">Designed & Developed By <a href="https://gdgjalandhar.com" target="_blank">GDG Jalandhar</a></p>
             </v-flex>
         </v-layout>
-    </v-container>
+    </v-container> -->
 </template>
 
 <script>
